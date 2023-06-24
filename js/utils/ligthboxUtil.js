58aeb944e4e3534590932d286b8e07691aae422c
kamga serges raoul <kamgakamga93@gmail.com>
2023-06-24 12:50:30 +0200

    update

--- js/utils/ligthboxUtil.js
+++ js/utils/ligthboxUtil.js
@@ -2,7 +2,7 @@ function buildImageLigthBox() {
     const boxContainer = document.createElement("div");
     boxContainer.classList.add("box-container");
     boxContainer.innerHTML =`
-    <img src="./assets/img/Marcel/Architecture_Dome.jpg" alt="une image"/>
+    <img src="assets/img/Marcel/Architecture_Dome.jpg" alt="une image"/>
     `;
     document.querySelector(".main-container").appendChild(boxContainer);
 }
